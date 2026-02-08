3cdf3ea9127c6068c16f604cb18306af51f32167
Farid333gdu <ebrahimifarid374@gmail.com>
2026-02-08 13:51:26 +0330

    Update stars5.py

--- stars5.py
+++ stars5.py
@@ -1917,4 +1917,4 @@ def set_transfer_group(message):
 
 
 # ================= اجرای ربات =================
-bot.infinity_polling(skip_pending=True)      pass
+bot.infinity_polling(skip_pending=True)      
